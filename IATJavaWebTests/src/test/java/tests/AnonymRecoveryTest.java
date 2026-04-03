9d67acac5becf013d7d7d0c0cb4e6f8b4b9551d0
IgorAkh <pe3k4bo0@ya.ru>
2026-04-03 06:25:14 +0300

    Added test

---
+++ IATJavaWebTests/src/test/java/tests/AnonymRecoveryTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import core.base.AnonymRecoveryPage;
+import core.base.BaseTest;
+import core.base.RecoveryByEmailPage;
+import core.base.RecoveryByPhonePage;
+import core.pages.LoginPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class AnonymRecoveryTest extends BaseTest {
+
+    private static LoginPage loginPage;
+    private static AnonymRecoveryPage anonymRecoveryPage;
+    private static RecoveryByPhonePage recoveryByPhonePage;
+    private static RecoveryByEmailPage recoveryByEmailPage;
+
+    @BeforeEach
+    public void prepare(){
+        open(baseUrl);
+        loginPage = new LoginPage();
+    }
+
+    @Test
+    public void anonymRecoveryByPhoneTest(){
+
+        loginPage.makeAccessToAnonymRecoveryPage();
+        anonymRecoveryPage = new AnonymRecoveryPage();
+        anonymRecoveryPage.goToPhoneRecovery();
+
+        recoveryByPhonePage = new RecoveryByPhonePage();
+        recoveryByPhonePage.verifyPageElements();
+
+    }
+
+    @Test
+    public void anonymRecoveryByEmailTest(){
+        loginPage.makeAccessToAnonymRecoveryPage();
+        anonymRecoveryPage = new AnonymRecoveryPage();
+        anonymRecoveryPage.goToEmailRecovery();
+
+        recoveryByEmailPage = new RecoveryByEmailPage();
+        recoveryByEmailPage.verifyPageElements();
+
+
+    }
+}
