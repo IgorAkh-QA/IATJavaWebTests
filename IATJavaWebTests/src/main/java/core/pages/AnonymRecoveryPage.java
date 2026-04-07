0d9fc7d1170dfb9575f919b99b93eda949b0cd74
IgorAkh <pe3k4bo0@ya.ru>
2026-04-07 07:36:15 +0300

    added close support window test

--- IATJavaWebTests/src/main/java/core/pages/AnonymRecoveryPage.java
+++ IATJavaWebTests/src/main/java/core/pages/AnonymRecoveryPage.java
@@ -1,9 +1,11 @@
-package core.base;
+package core.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import core.base.BasePage;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 
 
@@ -11,6 +13,9 @@ public class AnonymRecoveryPage extends BasePage {
     private SelenideElement phoneRecoveryButton = $("[data-l=\"t,phone\"]");
     private SelenideElement emailRecoveryButton = $("[data-l=\"t,email\"]");
     private SelenideElement supportMessageButton = $(".support-link_items");
+    private SelenideElement supportChatWindow = $("support-chat");
+    private SelenideElement exitSupportChatButton = supportChatWindow.$("[name^='ico_close']").closest("button");
+    private SelenideElement confirmExitSupportChatWindowButton = supportChatWindow.$("button").$(withText("Да"));
 
     {
         verifyPageElements();
@@ -34,10 +39,24 @@ public class AnonymRecoveryPage extends BasePage {
     }
 
     @Step("Нажимаем на кнопку перехода к обращения в техническую поддержку")
-    public void goToSupportPage(){
+    public void clickSupportMessageButton(){
         supportMessageButton.click();
     }
 
+    @Step("Проверяем, что окно чата технической поддержки отображается на странице")
+    public boolean isSupportChatVisible(){
+        return supportChatWindow.shouldBe(visible).exists();
+    }
+
+    @Step("Нажимаем на закрытие окна чата с поддержкой")
+    public void clickExitSupportChatButton(){
+        exitSupportChatButton.shouldBe(clickable).click();
+    }
+
+    @Step("Подтверждаем закрытие окна чата с поддержкой")
+    public void clickConfirmExitSupportChatWindowButton(){
+        confirmExitSupportChatWindowButton.shouldBe(interactable).click();
+    }
 
 
 }
