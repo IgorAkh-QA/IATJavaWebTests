420fa6c5bc9f23959d56939dfd0a30d9139d26ee
IgorAkh <pe3k4bo0@ya.ru>
2026-04-07 07:37:04 +0300

    Qr code test

---
+++ IATJavaWebTests/src/test/java/tests/QrCodeShouldBeVisibleTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import core.base.BaseTest;
+import core.pages.LoginPage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QrCodeShouldBeVisibleTest extends BaseTest {
+    private static LoginPage loginPage;
+    boolean expectedQrCodeVisibility = true;
+
+
+    @Test
+    public void qrCodeShouldBeVisibleTest(){
+        loginPage = new LoginPage();
+        loginPage.clickQrCodeLoginButton();
+        boolean isQrCodeVisible = loginPage.isQrCodeVisible();
+        assertEquals(expectedQrCodeVisibility, isQrCodeVisible, "Qr- code не найден на странице");
+
+    }
+
+}
