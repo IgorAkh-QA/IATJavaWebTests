0830db0da9d06e9b8447267e8714027224e8b674
IgorAkh <pe3k4bo0@ya.ru>
2026-04-07 07:34:36 +0300

    moved to correct directory

--- IATJavaWebTests/src/main/java/core/pages/RecoveryByEmailPage.java
+++ IATJavaWebTests/src/main/java/core/pages/RecoveryByEmailPage.java
@@ -1,11 +1,11 @@
-package core.base;
+package core.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import core.base.BasePage;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 
 
